cd5b275120d414dcfe6c184010afdb02253d5836
Zafer Steele <zafersteele@gmail.com>
2025-06-07 14:22:15 +0100

    Nornir automation examples

---
+++ nornir_netmiko_send_command_example.py
@@ -0,0 +1,9 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+
+nr = InitNornir()
+result = nr.run(
+    task= netmiko_send_command, command_string="show arp"   
+)
+print_result(result)
